5bbc53f3d1f2a6a42a92df81f2f941b11fdc8039
myrba2win <myrba2win@yahoo.com>
2020-07-29 00:46:36 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  The primary objective in this test is to write a couple of functions,
+##  namely "makeCacheMatrix" and "cacheSolve",
+##  which allow us to cache the inverse matrix entered in the first function.
 
-## Write a short comment describing this function
+##  Definitions:
+##      "x" is the input matrix
+##      "inv" is the solved value
+##      "mean" is changed to "inverse"
+makeCacheMatrix <- function(x = matrix()){
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
 
-makeCacheMatrix <- function(x = matrix()) {
-
+    get <- function()x
+    setInverse  <- function(inverse) inv <<- inverse
+    getInverse  <- function() inv
+    list(set = set, get = get,
+         setInverse  = setInverse,
+         getInverse  = getInverse)
 }
 
-
 ## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
+## IF it was already calculated, it saves it in cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+
+      message("getting cached result")
+      return(inv)
+    }
+    data <- x$get()
+    inv<- solve(data, ...)
+    x$setInverse(inv)
+    inv
 }
